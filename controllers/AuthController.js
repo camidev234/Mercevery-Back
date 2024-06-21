0a9b90e26df8d89c12856b70e97377dce328b6e3
Camidev234 <camilo222andresace@gmail.com>
2024-06-21 17:33:50 -0500

    comment the code in jwt

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,32 +7,43 @@ dotenv.config();
 
 class AuthController {
   static authUser = async (req, res) => {
+    // Destructuring the request body.
     const { email, password } = req.body;
     try {
+      // Query to get the user passed in request
       const query = "SELECT * FROM users WHERE email = ?";
+      // Execute the query
       const [rows] = await pool.query(query, [email]);
+      // The user find wll be the first element found
       const userFind = rows[0];
+      // If that user not found
       if (!userFind) {
         return res.status(404).json({
           message: "User not found",
         });
       }
 
+      // Else, validate the password compare the password passed in the request hash and query user hash
       const validPassword = await bcrypt.compare(password, userFind.password);
+      // if the passwords are not the same
       if (!validPassword) {
         return res.status(401).json({
           message: "Incorrect password, check your credentials",
         });
       }
 
+      // Else, create a token
+      // Receive the user id, the secret key and the expiration
       const token = jwt.sign({ id: userFind.id }, "mysecretkey123", {
         expiresIn: "1h",
       });
 
+      // Return the userfind 
       const userToReturn = await UserResource.user(userFind);
 
-      console.log(userToReturn);
+      // console.log(userToReturn);
 
+      // Response in JSON the user auth and access token
       res.json({
         access_token: token,
         user: userToReturn,
